dd897c39abaa103c60b3f44dbfc5b7359207f1d0
徐雁鑫 <funnycodingxu@gmail.com>
2020-07-25 23:58:03 +0800

    add more examples

---
+++ java_conccurency_practice/src/main/java/chapter4/Test.java
@@ -0,0 +1,17 @@
+package chapter4;
+
+import java.util.HashMap;
+
+/**
+ * @author XuYanXin
+ * @program javaconcurrency_learn
+ * @description
+ * @date 2020/4/3 9:50 上午
+ */
+
+public class Test {
+    public static void main(String[] args) {
+        HashMap<Object, Object> objectObjectHashMap = new HashMap<>();
+        objectObjectHashMap.put("1", "2");
+    }
+}
