c923a3af834f07c17eb27b9ef16b2d56b60f2906
徐雁鑫 <funnycodingxu@gmail.com>
2020-08-20 23:25:40 +0800

    update 33 code

--- geektime_javaconcurrency/src/main/java/chapter3/Singleton.java
+++ geektime_javaconcurrency/src/main/java/chapter3/Singleton.java
@@ -1,5 +1,15 @@
 package chapter3;
 
+import co.paralleluniverse.fibers.Fiber;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author XuYanXin
  * @program javaconcurrency_learn
@@ -9,15 +19,17 @@ package chapter3;
 
 public class Singleton {
     private static Singleton singleton;
+
     // 私有化构造函数
     private Singleton() {
 
     }
+
     // 单例的获取 Singleton 实例的方法
-    public   static Singleton getInstance() {
+    public static Singleton getInstance() {
         // 第一次检查
         if (singleton == null) {
-            synchronized (Singleton.class){
+            synchronized (Singleton.class) {
                 // 获取锁后的二次检查
                 if (singleton == null) {
                     singleton = new Singleton();
@@ -25,5 +37,39 @@ public class Singleton {
             }
         }
         return singleton;
+    }
+
+    public static void main(String[] args) throws IOException {
+        final ServerSocketChannel ssc = ServerSocketChannel.open()
+                .bind(new InetSocketAddress(8080));
+
+        // 处理请求
+        try {
+            while (true) {
+                // 接受请求
+                SocketChannel sc = ssc.accept();
+                // 这里使用 Fiber.schedule 创建一个协程
+                Fiber.schedule(() -> {
+                    try {
+                        // 读 Socket
+                        ByteBuffer rb = ByteBuffer.allocate(1024);
+                        sc.read(rb);
+                        // 模拟处理请求
+                        LockSupport.parkNanos(2000 * 1000000);
+                        // 写 Socket
+                        ByteBuffer wb = (ByteBuffer) rb.flip();
+                        sc.write(wb);
+                        sc.close();
+                    } catch (Exception e) {
+                        throw new UncheckedIOException(e);
+                    }
+                }).start();
+            }
+        }finally {
+            ssc.close();
+        }
+
+
+
     }
 }
